d258c4d93d82481f9e515bb5cc1e55832cbd8619
Ken980 <kenboy9800@gmail.com>
2022-03-16 21:37:19 +0700

    add invert grayscale value
    change from white = 255 and black = 0 to white = 0 and black = 255

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 from PIL import Image
 from PIL import ImageOps
 
-image = Image.open("dataset/numbers/9/1.png")
+image = Image.open("test/test.png")
 image = ImageOps.grayscale(image)
 image = image.resize((28,28))
 
 array = []
 array_2d = []
+
+
+# create 2d array
 
 for i in range(28):
     array_2d.append([])
@@ -17,9 +20,22 @@ for i in range(28):
         array.append(pixel)
         array_2d[i].append(pixel)
 
+'''
 for i in range(28):
     print(array_2d[i])
+'''
 
+'''
 print("\n")
 print(array)
 print(array_2d)
+'''
+
+#invert grayscale value
+
+for i in range(784):
+    array[i] = array[i] - 127.5
+    array[i] = -array[i]
+    array[i] = int(array[i] + 127.5)
+
+print(array)
