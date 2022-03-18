5adcac49378fa7b85b871f50626eb8dadcfcaa8a
Ken980 <kenboy9800@gmail.com>
2022-03-18 08:28:46 +0700

    create NN layers
    create a framework to make a neural network

--- test.py
+++ test.py
@@ -1,13 +1,34 @@
 from PIL import Image
 from PIL import ImageOps
+import math
 
 image = Image.open("test/test.png")
 image = ImageOps.grayscale(image)
 image = image.resize((28,28))
 
-array = []
+array = []      # input
 array_2d = []
 
+# layers = [[activations],[weights],[bias]]
+# the number is random for now
+
+layer_1 = [[0.72,0.23,0.76], [3,2,-4], [8,-6,4]]    # 3 activations
+layer_2 = [[0.55,0.31], [-2,3], [7,0.2]]    # 2 activations
+layer_3 = [[0.12,0.7,0.42,0.98], [2,-4,-2,1], [1,2,3,4]]    # 4 activations
+
+hidden_layers = [layer_1,layer_2,layer_3]
+
+output = [[0.7,0.4,0.3,0.15], [2,3,1,4], [-3,-5,6,2]] # 4 output for simplicity for now
+
+print("hidden layer",hidden_layers)
+
+network = hidden_layers
+
+print("network before",network)
+network.append(output)  # network[layer][activation/weight/bias][index]
+
+print("network after",network)
+print(network[0][0][1])
 
 # create 2d array
 
@@ -37,5 +58,19 @@ for i in range(784):
     array[i] = array[i] - 127.5
     array[i] = -array[i]
     array[i] = int(array[i] + 127.5)
+    array[i] = array[i] * (1/255)           # change value from 0 to 255, to between 0 and 1
 
 print(array)
+
+network.insert(0,array)
+
+def sigmoid(x):
+    x = 1/(1+((math.e)**(-x)))
+    return x
+
+sum = 0
+for i in array:
+    sum = sum + i
+
+print(sum)
+print(network)
